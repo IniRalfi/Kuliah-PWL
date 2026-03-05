37f26bf9fedce28e0ed68ec4fd8d284b7fc00fd8
Rafli Pratama <rflipratm@gmail.com>
2026-03-05 10:04:58 +0700

    PPWL4: PRAKTIKUM 1 - VALIDASI REQUEST BODY

---
+++ PPWL4/src/validation.ts
@@ -0,0 +1,46 @@
+import { Elysia, t } from "elysia";
+import { openapi } from "@elysiajs/openapi";
+
+const app = new Elysia()
+  .use(openapi())
+  .post(
+    "/request",
+    ({ body }) => {
+      return {
+        message: "Success",
+        data: body,
+      };
+    },
+    {
+      body: t.Object({
+        name: t.String({ minLength: 3 }),
+        email: t.String({ format: "email" }),
+        age: t.Number({ minimum: 18 }),
+      }),
+    },
+  )
+  // Query dan Params
+  .get("/id/:id", () => "Hello World!", {
+    query: t.Object({
+      name: t.String(),
+    }),
+    params: t.Object({
+      id: t.Number(),
+    }),
+  })
+  // Contoh Params
+  .get("/user/:id", ({ params }) => params, {
+    params: t.Object({
+      id: t.Number(),
+    }),
+  })
+  .get("/search", ({ query }) => query, {
+    query: t.Object({
+      keyword: t.String(),
+      page: t.Optional(t.Number()),
+    }),
+  })
+
+  .listen(3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
